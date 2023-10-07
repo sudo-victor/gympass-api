e2de5ce4848e21baa26bdf894791ea386d9d01b0
Victor de Souto <victor.souto@hotmail.com>
2023-10-07 17:36:26 -0300

    chore: add refresh token

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,35 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { InvalidCredentialsError } from '@/usecases/errors/invalid-credentials-error';
+import { makeAuthenticateUseCase } from '@/usecases/factories/make-authenticate-use-case';
+
+export async function refresh(request: FastifyRequest, reply: FastifyReply) {
+    await request.jwtVerify({ onlyCookie: true });
+
+    const token = await reply.jwtSign({}, {
+        sign: {
+            sub: request.user.sub
+        }
+    });
+
+    const refreshToken = await reply.jwtSign({}, {
+        sign: {
+            sub: request.user.sub,
+            expiresIn: '7d'
+        }
+    });
+
+    return reply
+        .setCookie('refreshToken', refreshToken, {
+            path: '/',
+            secure: true,
+            sameSite: true,
+            httpOnly: true
+        })
+        .status(200)
+        .send({
+            token
+        });
+
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { register } from './register';
 import { authenticate } from './authenticate';
 import { profile } from './profile';
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
+import { refresh } from './refresh';
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', register);
     app.post('/sessions', authenticate);
     app.get('/me', { onRequest: [verifyJWT] }, profile);
+    app.patch('/token/refresh', refresh);
 }
