f69c5067a52b29f27a42fa438811cfed8e92024c
Victor de Souto <victor.souto@hotmail.com>
2023-10-07 17:57:49 -0300

    fix: tests with rbac

--- src/utils/tests/create-and-authenticate-user.ts
+++ src/utils/tests/create-and-authenticate-user.ts
@@ -1,13 +1,16 @@
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 import request from 'supertest';
 
-export async function createAndAuthenticateUser(app) {
-    await request(app.server)
-        .post('/users')
-        .send({
+export async function createAndAuthenticateUser(app, isAdmin = false) {
+    const user = await prisma.user.create({
+        data: {
             name: 'John Doe',
             email: 'johndoe@example.com',
-            password: '123456'
-        });
+            password_hash: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER'
+        }
+    });
 
     const authResponse = await request(app.server)
         .post('/sessions')
